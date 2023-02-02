2a29513959a825c087873e78b783006f739a5ac9
javielsitorico <zx21student030@colegiovalledelmiro.es>
2023-02-02 09:45:28 +0100

    Proyecto tutorial terminado

--- agenda/urls.py
+++ agenda/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
      path('', views.EmpresaListView.as_view(), name='listado'),
      path('empresa/<int:pk>', views.EmpresaDetailView.as_view(), name='detalle'),
+     path('crear', views.EmpresaCreateView.as_view(), name='crear'),
+     path('modificar/<int:pk>', views.EmpresaUpdateView.as_view(), name='modificar'),
+     path('borrar/<int:pk>', views.EmpresaDeleteView.as_view(), name='borrar'),
 ]
--- agenda/views.py
+++ agenda/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.template import loader
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Empresa
 
 class EmpresaListView(ListView):
      model = Empresa
      # context_object_name = 'empresas'
      
-class EmpresaDetailView(DetailView, ):
+class EmpresaDetailView(DetailView):
      model = Empresa
+     
+class EmpresaCreateView(CreateView):
+     model = Empresa
+     fields = ['nombre', 'tipo', 'direccion', 'telefono', 'personaContacto']
+     success_url = reverse_lazy('listado')
+
+class EmpresaUpdateView(UpdateView):
+     model = Empresa
+     fields = ['nombre', 'tipo', 'direccion', 'telefono', 'personaContacto']
+     template_name_suffix = '_update'
+     success_url = reverse_lazy('listado')
+
+class EmpresaDeleteView(DeleteView):
+     model = Empresa
+     success_url = reverse_lazy('listado')
+
 
 # Esta seria la manera de hacerlo que conocemos
 #
